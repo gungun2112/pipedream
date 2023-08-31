84a66a6012acc2faf71de4602ed1da53fe60f2b2
Andrew Chuang <andrewjschuang@gmail.com>
2023-08-31 13:40:17 -0300

    [WIP] adapt polling tests with common files

--- packages/component_code_gen/tests/polling_sources/apps.py
+++ packages/component_code_gen/tests/polling_sources/apps.py
@@ -2,71 +2,113 @@ apps = [
     {
         'app': 'a123formbuilder',
         'instructions': 'how to emit events for every new form submission',
-        'key': 'a123formbuilder-form-response-submitted'
+        'key': 'a123formbuilder-form-response-submitted',
+        'common-files': [
+            'a123formbuilder/a123formbuilder.app.mjs',
+        ],
     },
     {
         'app': 'bigml',
         'instructions': 'how to emit events for every new model created',
-        'key': 'bigml-new-model-created'
+        'key': 'bigml-new-model-created',
+        'common-files': [
+            'bigml/bigml.app.mjs',
+        ],
     },
     {
         'app': 'coda',
         'instructions': 'how to emit events for every created or updated row',
-        'key': 'coda-new-row-created'
+        'key': 'coda-new-row-created',
+        'common-files': [
+            'coda/coda.app.mjs',
+        ],
     },
     {
         'app': 'docusign',
         'instructions': 'how to emit events when an envelope status is set to sent or complete',
-        'key': 'docusign-envelope-sent-or-complete'
+        'key': 'docusign-envelope-sent-or-complete',
+        'common-files': [
+            'docusign/docusign.app.mjs',
+        ],
     },
     {
         'app': 'drata',
         'instructions': 'how to emit events whenever a monitor fails',
-        'key': 'drata-failed-monitor'
+        'key': 'drata-failed-monitor',
+        'common-files': [
+            'drata/drata.app.mjs',
+        ],
     },
     {
         'app': 'faunadb',
         'instructions': 'how to emit events each time you add or remove a document from a specific collection',
-        'key': 'faunadb-changes-to-collection'
+        'key': 'faunadb-changes-to-collection',
+        'common-files': [
+            'faunadb/faunadb.app.mjs',
+        ],
     },
     {
         'app': 'here',
         'instructions': 'how to emit weather reports for a specific zip code on a schedule',
-        'key': 'here-weather-for-zip'
+        'key': 'here-weather-for-zip',
+        'common-files': [
+            'here/here.app.mjs',
+        ],
     },
     {
         'app': 'hubspot',
         'instructions': 'how to emit events for each new deal created',
-        'key': 'hubspot-new-deal'
+        'key': 'hubspot-new-deal',
+        'common-files': [
+            'hubspot/hubspot.app.mjs',
+        ],
     },
     {
         'app': 'intercom',
         'instructions': 'how to emit events each time a user replies to a conversation',
-        'key': 'intercom-new-user-reply'
+        'key': 'intercom-new-user-reply',
+        'common-files': [
+            'intercom/intercom.app.mjs',
+        ],
     },
     {
         'app': 'mailchimp',
         'instructions': 'how to emit events when a recipient clicks a pre-specified link in an specific campaign',
-        'key': 'mailchimp-link-clicked'
+        'key': 'mailchimp-link-clicked',
+        'common-files': [
+            'mailchimp/mailchimp.app.mjs',
+        ],
     },
     {
         'app': 'monday',
         'instructions': 'how to emit events when a new board is created in Monday',
-        'key': 'monday-new-board'
+        'key': 'monday-new-board',
+        'common-files': [
+            'monday/monday.app.mjs',
+        ],
     },
     {
         'app': 'notion',
         'instructions': 'how to emit events when a page in a database is updated',
-        'key': 'notion-updated-page-in-database'
+        'key': 'notion-updated-page-in-database',
+        'common-files': [
+            'notion/notion.app.mjs',
+        ],
     },
     {
         'app': 'raindrop',
         'instructions': 'how to emit events when a bookmark is added',
-        'key': 'raindrop-new-bookmark'
+        'key': 'raindrop-new-bookmark',
+        'common-files': [
+            'raindrop/raindrop.app.mjs',
+        ],
     },
     {
         'app': 'supabase',
         'instructions': 'how to emit events for every new row added in a table',
-        'key': 'supabase-new-row-added'
+        'key': 'supabase-new-row-added',
+        'common-files': [
+            'supabase/supabase.app.mjs',
+        ],
     },
 ]
