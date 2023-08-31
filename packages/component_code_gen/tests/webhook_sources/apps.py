13da498021f4058cb17b450c2434c75500205b84
Andrew Chuang <andrewjschuang@gmail.com>
2023-08-31 13:39:54 -0300

    [WIP] adapt webhooks tests with common files

--- packages/component_code_gen/tests/webhook_sources/apps.py
+++ packages/component_code_gen/tests/webhook_sources/apps.py
@@ -2,71 +2,113 @@ apps = [
     {
         'app': 'accelo',
         'instructions': 'how to get webhooks for every new assigned task',
-        'key': 'accelo-new-task-assigned'
+        'key': 'accelo-new-task-assigned',
+        'common-files': [
+            'accelo/accelo.app.mjs',
+        ],
     },
     {
         'app': 'asana',
         'instructions': 'how to get webhooks for every new project',
-        'key': 'asana-new-project'
+        'key': 'asana-new-project',
+        'common-files': [
+            'asana/asana.app.mjs',
+        ],
     },
     {
         'app': 'brex',
         'instructions': 'how to get webhooks for every new transfer event',
-        'key': 'brex-new-transfer-event'
+        'key': 'brex-new-transfer-event',
+        'common-files': [
+            'brex/brex.app.mjs',
+        ],
     },
     {
         'app': 'fibery',
         'instructions': 'how to get webhooks for every new created entity',
-        'key': 'fibery-entity-created'
+        'key': 'fibery-entity-created',
+        'common-files': [
+            'fibery/fibery.app.mjs',
+        ],
     },
     {
         'app': 'github',
         'instructions': 'how to get webhooks for every new commit',
-        'key': 'github-new-commit'
+        'key': 'github-new-commit',
+        'common-files': [
+            'github/github.app.mjs',
+        ],
     },
     {
         'app': 'postmark',
         'instructions': 'how to get webhooks for every new inbound email',
-        'key': 'postmark-new-inbound-email-received'
+        'key': 'postmark-new-inbound-email-received',
+        'common-files': [
+            'postmark/postmark.app.mjs',
+        ],
     },
     {
         'app': 'process_street',
         'instructions': 'how to get webhooks for every new completed workflow run',
-        'key': 'process_street-workflow-run-completed'
+        'key': 'process_street-workflow-run-completed',
+        'common-files': [
+            'process_street/process_street.app.mjs',
+        ],
     },
     {
         'app': 'quaderno',
         'instructions': 'how to get webhooks for every new received payment',
-        'key': 'quaderno-payment-received'
+        'key': 'quaderno-payment-received',
+        'common-files': [
+            'quaderno/quaderno.app.mjs',
+        ],
     },
     {
         'app': 'shipcloud',
         'instructions': 'how to get webhooks for every new shipment status',
-        'key': 'shipcloud-new-shipment-status'
+        'key': 'shipcloud-new-shipment-status',
+        'common-files': [
+            'shipcloud/shipcloud.app.mjs',
+        ],
     },
     {
         'app': 'stripe',
         'instructions': 'how to get webhooks for every new payment',
-        'key': 'stripe-new-payment'
+        'key': 'stripe-new-payment',
+        'common-files': [
+            'stripe/stripe.app.mjs',
+        ],
     },
     {
         'app': 'tally',
         'instructions': 'how to get webhooks for every new response',
-        'key': 'tally-new-response'
+        'key': 'tally-new-response',
+        'common-files': [
+            'tally/tally.app.mjs',
+        ],
     },
     {
         'app': 'twilio',
         'instructions': 'how to get webhooks for every new call',
-        'key': 'twilio-new-call'
+        'key': 'twilio-new-call',
+        'common-files': [
+            'twilio/twilio.app.mjs',
+        ],
     },
     {
         'app': 'woocommerce',
         'instructions': 'how to get webhooks for every new order event',
-        'key': 'woocommerce-new-order-event'
+        'key': 'woocommerce-new-order-event',
+        'common-files': [
+            'woocommerce/woocommerce.app.mjs',
+        ],
     },
     {
         'app': 'zenkit',
         'instructions': 'how to get webhooks for every new notification',
-        'key': 'zenkit-new-notification'
+        'key': 'zenkit-new-notification',
+        'common-files': [
+            'zenkit/zenkit.app.mjs',
+        ],
     },
 ]
